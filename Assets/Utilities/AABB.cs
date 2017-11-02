c2a9ac6169169d49bf651e6c68fdaa32349348aa
Nicholas Arnaud <arnaudnicholas@gmail.com>
2017-11-02 17:44:54 -0500

    Fixed square collision checks

---
+++ Assets/Utilities/AABB.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+public class AABB : MonoBehaviour
+{
+    public Vector2 _min;
+    public Vector2 _max;
+    private Vector2 gameObjectPos;
+    private Vector2 gameObjectScale;
+    public new string name;
+
+    public bool TestOnLap(AABB boxCollider, AABB boxCollidie)
+    {
+        float d1x = boxCollidie._min.x - boxCollider._max.x;
+        float d1y = boxCollidie._min.y - boxCollider._max.y;
+        float d2x = boxCollider._min.x - boxCollidie._max.x;
+        float d2y = boxCollider._min.y - boxCollidie._max.y;
+
+        if (d1x > 0 || d1y > 0)
+            return false;
+        if (d2x > 0 || d2y > 0)
+            return false;
+        return true;
+    }
+
+    void Start()
+    {
+        name = gameObject.name;
+    }
+    void Update()
+    {
+        gameObjectPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        gameObjectScale = new Vector2(gameObject.transform.localScale.x, gameObject.transform.localScale.y);
+        _min = gameObjectPos - gameObjectScale / 2;
+        _max = gameObjectPos + gameObjectScale / 2;
+    }
+}
--- Assets/Utilities/ColliderCheck.cs
+++ Assets/Utilities/ColliderCheck.cs
@@ -1,21 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ColliderCheck : MonoBehaviour
 {
-    public Utilities Collide;
-	// Use this for initialization
-	void Start () {
-        Collide = new Utilities();
-		Utilities.AABB Box1 = new Utilities.AABB(new Vector2(0,0), new Vector2(1,1));
-        Utilities.AABB Box2 = new Utilities.AABB(new Vector2(0.5f, 0.5f), new Vector2(1.5f, 1.5f));
-	    bool isHitting =Collide.TestOnLap(Box1, Box2);
-	    int one = 1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool colliding;
+    [SerializeField]
+    private List<AABB> axisList;
+    private Vector2 gameObjectPos;
+    private Vector2 gameObjectScale;
+    [SerializeField]
+    List<AABB> ActiveList = new List<AABB>();
+
+    // Use this for initialization
+    void Start()
+    {
+        axisList = new List<AABB>();
+
+        axisList.AddRange(FindObjectsOfType<AABB>());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActiveList.Clear();
+        axisList.Sort((ob, ob2) => ob._min.x.CompareTo(ob2._min.x)); //biggest to smallest        
+        ActiveList.Add(axisList[0]);
+
+        for (int i = 0; i < axisList.Count; i++)
+        {
+            for (int j = i + 1; j < axisList.Count; j++)
+            {
+                if (axisList[j]._min.x > axisList[i]._max.x)
+                {
+                    ActiveList.Remove(axisList[i]);
+                    break;
+                }
+                if (axisList[j]._min.y > axisList[i]._max.y)
+                {
+                    ActiveList.Add(axisList[i]);
+                    ActiveList.Remove(axisList[j]);
+                    continue;
+                }
+                colliding = axisList[0].TestOnLap(axisList[i], axisList[j]);
+
+            }
+        }
+        Debug.Log("NumOfAABBInActive: " + ActiveList.Count);
+    }
 }
